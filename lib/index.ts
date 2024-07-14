02f9dbc6207bb618205ebf7e74fa0990b0cab516
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-07-14 17:27:36 +0700

    chore: fix lint errors

--- lib/index.ts
+++ lib/index.ts
@@ -4,43 +4,44 @@ import { ActorHttpFetch } from '@comunica/actor-http-fetch';
 import { ActorHttp } from '@comunica/bus-http';
 
 async function run() {
-    for (const Actor of [
-        ActorHttpFetch,
-        ActorHttpNative
-    ]) {
-        for (const url of ['https://www.rubensworks.net/', 'http://example.org']) {
-            const times: [number, number, number][] = [];
-            for (let i = 0; i < 100; i++) {
-                const fetch = new Actor({
-                    name: 'actor-http',
-                    bus: new Bus({
-                        name: 'bus',
-                    }),
-                });
+  for (const Actor of [
+    ActorHttpFetch,
+    ActorHttpNative,
+  ]) {
+    for (const url of ['https://www.rubensworks.net/', 'http://example.org']) {
+      const times: [number, number, number][] = [];
+      for (let i = 0; i < 100; i += 1) {
+        const fetch = new Actor({
+          name: 'actor-http',
+          bus: new Bus({
+            name: 'bus',
+          }),
+        });
 
-                let tim: [number, number, number] = [0, 0, 0]
-                times.push(tim);
+        const tim: [number, number, number] = [0, 0, 0];
+        times.push(tim);
 
-                const startTime = Date.now();
-                const res = await fetch.run({ input: url, context: new ActionContext() });
-                const endTime = Date.now();
-                tim[0] = endTime - startTime;
+        const startTime = Date.now();
+        // eslint-disable-next-line no-await-in-loop
+        const res = await fetch.run({ input: url, context: new ActionContext() });
+        const endTime = Date.now();
+        tim[0] = endTime - startTime;
 
-                ActorHttp.toNodeReadable(res.body).on('data', (data: any) => {
-                    tim[1] = Date.now() - endTime;
-                }).on('end', () => {
-                    tim[2] = Date.now() - endTime;
-                });
-            }
+        ActorHttp.toNodeReadable(res.body).on('data', () => {
+          tim[1] = Date.now() - endTime;
+        }).on('end', () => {
+          tim[2] = Date.now() - endTime;
+        });
+      }
 
-            setTimeout(() => {
-                console.log(Actor, url)
-                console.log(times.map(t => t[0]).reduce((a, b) => a + b) / times.length);
-                console.log(times.map(t => t[1]).reduce((a, b) => a + b) / times.length);
-                console.log(times.map(t => t[2]).reduce((a, b) => a + b) / times.length);
-            }, 500);
-        }
+      setTimeout(() => {
+        console.log(Actor, url);
+        console.log(times.map((t) => t[0]).reduce((a, b) => a + b) / times.length);
+        console.log(times.map((t) => t[1]).reduce((a, b) => a + b) / times.length);
+        console.log(times.map((t) => t[2]).reduce((a, b) => a + b) / times.length);
+      }, 500);
     }
+  }
 }
 
 run();
